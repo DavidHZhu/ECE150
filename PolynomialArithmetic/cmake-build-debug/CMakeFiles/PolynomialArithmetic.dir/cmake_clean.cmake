08eccff60ccbb9070591fa4f62a4ed7d9aefb9da
David Zhu <dhzhu@uwaterloo.ca>
2018-11-11 18:46:43 -0500

    Initial Commit
    Hello World default

---
+++ PolynomialArithmetic/cmake-build-debug/CMakeFiles/PolynomialArithmetic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PolynomialArithmetic.dir/main.cpp.o"
+  "PolynomialArithmetic.pdb"
+  "PolynomialArithmetic.exe"
+  "libPolynomialArithmetic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PolynomialArithmetic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
