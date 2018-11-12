14769324874b8042e2b63293eb1d85eb486597fe
David Zhu <dhzhu@uwaterloo.ca>
2018-11-11 20:17:51 -0500

    changed main.cpp name

---
+++ PolynomialArithmetic/Polynomial.cpp
@@ -0,0 +1,18 @@
+// Title: Polynomial Arithmetic Project
+// Author: David Zhu
+// Description: Interval calculator using classes
+// Updates: Oct 20: Classes and functions setup(check for initialization and divide by 0
+//                  Working: exit, enter, negate, ms, mr, mc, m+ (mplus), m- (msubt),
+//                  scalar_add, scalar_subtract, scalar_multiply, scalar_divide, scalar_divided_by
+//          Oct 21: interval_add, interval_subtract, interval_multiply, interval_divide
+//                  intersect, integers and cartesian_integers
+// Problems: Need to check and swap if the numbers after an + or - (fixed)
+//
+//=================================================================================================
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+//#include "Polynomial.h"
+// Class Declaration
+//=================================================================================================
