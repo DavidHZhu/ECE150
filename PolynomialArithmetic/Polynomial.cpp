599553879228e69e865ab4069d54b6d932022d6a
David Zhu <dhzhu@uwaterloo.ca>
2018-11-12 16:20:58 -0500

    Commit suicide

--- PolynomialArithmetic/Polynomial.cpp
+++ PolynomialArithmetic/Polynomial.cpp
@@ -1,18 +1,37 @@
 // Title: Polynomial Arithmetic Project
 // Author: David Zhu
-// Description: Interval calculator using classes
-// Updates: Oct 20: Classes and functions setup(check for initialization and divide by 0
-//                  Working: exit, enter, negate, ms, mr, mc, m+ (mplus), m- (msubt),
-//                  scalar_add, scalar_subtract, scalar_multiply, scalar_divide, scalar_divided_by
-//          Oct 21: interval_add, interval_subtract, interval_multiply, interval_divide
-//                  intersect, integers and cartesian_integers
-// Problems: Need to check and swap if the numbers after an + or - (fixed)
+// Description: Polynomial calculator using pointers and structures
+// Updates: Nov 12: Added header file and begun on functions
+// To do: figure out whether or not to delete in init_poly
+//
+//
 //
 //=================================================================================================
 #include <iostream>
 #include <cmath>
 #include <string>
 #include <algorithm>
-//#include "Polynomial.h"
-// Class Declaration
+#include "Polynomial.h"
 //=================================================================================================
+
+
+int main(){
+
+    return 0;
+}
+
+void init_poly( poly_t &p, double const init_coeffs[], unsigned int const init_degree ){
+
+    // Checks if the previous array was deallocated:
+    if (p.a_coeffs != nullptr){
+        delete p.a_coeffs;
+    }
+
+    // Allocates memory dynamically for new array:
+    double *new_poly = new double[init_degree+1];
+    for (int i = 0; i <= init_degree; ++i){
+        new_poly[i] = init_coeffs[i];
+    }
+    p.a_coeffs = new_poly;
+
+}
