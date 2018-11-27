0ce72e478d288167c5098c934bdeaa5fbd0aa9ba
David Zhu <dhzhu@uwaterloo.ca>
2018-11-26 21:42:03 -0500

    Created Project
    First init

---
+++ LinkedLists/cmake-build-debug/CMakeFiles/LinkedLists.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkedLists.dir/main.cpp.o"
+  "LinkedLists.pdb"
+  "LinkedLists.exe"
+  "libLinkedLists.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinkedLists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
